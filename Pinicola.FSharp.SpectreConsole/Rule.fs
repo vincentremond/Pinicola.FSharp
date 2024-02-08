ee9e03e909b780c5bdb0eede8214d5151476af37
Vincent Rémond <vincent.remond@gmail.com>
2024-02-08 14:20:19 +0100

    Add Rule and Style modules

---
+++ Pinicola.FSharp.SpectreConsole/Rule.fs
@@ -0,0 +1,11 @@
+﻿namespace Pinicola.FSharp.SpectreConsole
+
+[<RequireQualifiedAccess>]
+module Rule =
+    open Spectre.Console
+
+    let init () = Rule()
+
+    let setStyle style (rule: Rule) =
+        rule.Style <- style
+        rule
---
+++ Pinicola.FSharp.SpectreConsole/Style.fs
@@ -0,0 +1,31 @@
+﻿namespace Pinicola.FSharp.SpectreConsole
+
+[<RequireQualifiedAccess>]
+module Style =
+
+    open Spectre.Console
+
+    let init foreground background decoration link =
+        Style(foreground |> Option.toNullable, background |> Option.toNullable, decoration |> Option.toNullable, link |> Option.toObj)
+
+type StyleBuilder = {
+    Foreground: Spectre.Console.Color option
+    Background: Spectre.Console.Color option
+    Decoration: Spectre.Console.Decoration option
+    Link: string option
+} with
+
+    static member default_ = {
+        Foreground = None
+        Background = None
+        Decoration = None
+        Link = None
+    }
+    
+    static member withForeground color this = { this with Foreground = Some color }
+    static member withBackground color this = { this with Background = Some color }
+    static member withDecoration decoration this = { this with Decoration = Some decoration }
+    static member withLink link this = { this with Link = Some link }
+
+    static member build this =
+        Style.init this.Foreground this.Background this.Decoration this.Link
