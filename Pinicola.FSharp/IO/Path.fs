ae5f56125bcc8b156df53c99696310438c13c922
Vincent Rémond <vincent.remond@gmail.com>
2025-01-01 22:09:30 +0100

    fix & feat

--- Pinicola.FSharp/IO/Path.fs
+++ Pinicola.FSharp/IO/Path.fs
@@ -3,6 +3,10 @@
 open System.IO
 
 [<AutoOpen>]
-module Path =
+module PathOperators =
 
     let (</>) (path1: string) (path2: string) = Path.Join(path1, path2)
+
+[<RequireQualifiedAccess>]
+module Path =
+    let getFullPath path = Path.GetFullPath path
