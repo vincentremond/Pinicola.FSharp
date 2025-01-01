89f522c2690d7ec8fd5d3321d957b67128dfbfa6
Vincent Rémond <vincent.remond@gmail.com>
2025-01-01 21:21:19 +0100

    update tools

--- Pinicola.FSharp/String.fs
+++ Pinicola.FSharp/String.fs
@@ -2,6 +2,12 @@
 
 open System
 
+[<AutoOpen>]
+module StringActivePatterns =
+
+    let (|Contains|_|) comparisonType (s: string) (sub: string) =
+        if s.Contains(sub, comparisonType) then Some() else None
+
 [<RequireQualifiedAccess>]
 module String =
 
@@ -22,3 +28,18 @@ module String =
         equalsWithComparison StringComparison.InvariantCultureIgnoreCase
 
     let equalsInvariantCulture = equalsWithComparison StringComparison.InvariantCulture
+
+    let split (separator: char) (s: string) = s.Split(separator) |> List.ofArray
+
+    let trim (s: string) = s.Trim()
+    let trimChar (c: char) (s: string) = s.Trim(c)
+
+    let trimEnd (s: string) = s.TrimEnd()
+
+    let trimEndChar (c: char) (s: string) = s.TrimEnd(c)
+
+    let trimStart (s: string) = s.TrimStart()
+
+    let trimStartChar (c: char) (s: string) = s.TrimStart(c)
+
+    let toLower (s: string) = s.ToLower()
