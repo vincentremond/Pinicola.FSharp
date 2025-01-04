9727eadcdc4e773a37791619e33739fd69512b4e
Vincent Rémond <vincent.remond@gmail.com>
2025-01-04 14:04:32 +0100

    update lib

---
+++ Pinicola.FSharp/FParsec/FParsecHelper.fs
@@ -0,0 +1,12 @@
+﻿namespace Pinicola.FSharp.FParsec
+
+[<AutoOpen>]
+module FParsecHelper =
+
+    open FParsec
+
+    let parseOrFail parser input =
+        match run parser input with
+        | Success(result, _userState, _position) -> result
+        | Failure(errorMsg, _parserError, _userState) -> failwithf $"Failed to parse input: %s{errorMsg}"
+
---
+++ Pinicola.FSharp/Map.fs
@@ -0,0 +1,10 @@
+﻿namespace Pinicola.FSharp
+
+[<RequireQualifiedAccess>]
+module Map =
+
+    let tryFindOrDefault key defaultValue map =
+        match Map.tryFind key map with
+        | Some value -> value
+        | None -> defaultValue
+
