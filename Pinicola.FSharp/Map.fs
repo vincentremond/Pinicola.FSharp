7da52419a473755528aa1c4c2b51e6896b5fb8bb
Vincent Rémond <vincent.remond@gmail.com>
2025-01-23 09:28:46 +0100

    feat: Implement safeOfSeq function in Map module and update project file for Map.fs inclusion

--- Pinicola.FSharp/Map.fs
+++ Pinicola.FSharp/Map.fs
@@ -8,3 +8,10 @@ module Map =
         | Some value -> value
         | None -> defaultValue
 
+    let safeOfSeq (seq: ('a * 'b) seq) : Map<'a, 'b> =
+        let folder acc (k, v) =
+            match Map.containsKey k acc with
+            | true -> failwithf $"Duplicate key: %A{k}"
+            | false -> Map.add k v acc
+
+        seq |> Seq.fold folder Map.empty
