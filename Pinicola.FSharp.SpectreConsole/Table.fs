0ab3057d69bbc0b13b0a1f1f3de18d098b1cc1b4
Vincent Rémond <vincent.remond@gmail.com>
2025-01-29 12:32:36 +0100

    feat: Add Table module for Spectre.Console integration
    - Introduced new Table.fs module with helper functions
    - Implemented init, addColumns, and addRow functions
    - Updated project file to include new Table module

---
+++ Pinicola.FSharp.SpectreConsole/Table.fs
@@ -0,0 +1,14 @@
+﻿namespace Pinicola.FSharp.SpectreConsole
+
+open Spectre.Console
+
+[<RequireQualifiedAccess>]
+module Table =
+
+    let init () = Table()
+
+    let addColumns (columns: string list) (table: Table) =
+        table.AddColumns(columns |> List.toArray)
+        
+    let addRow (row: string list) (table: Table) =
+        table.AddRow(row |> List.toArray)
