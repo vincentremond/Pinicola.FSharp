48cec4b7325350a716de69124ff6a4b31d769c68
Vincent Rémond <vincent.remond@gmail.com>
2025-01-29 12:27:14 +0100

    feat: Add utility functions to Map and Tuple2 modules

--- Pinicola.FSharp/Map.fs
+++ Pinicola.FSharp/Map.fs
@@ -15,3 +15,18 @@ module Map =
             | false -> Map.add k v acc
 
         seq |> Seq.fold folder Map.empty
+
+    let keySet (map: Map<'k, 'v>) : Set<'k> = map |> Map.keys |> Set.ofSeq
+
+    let merge (map1: Map<'k, 'v1>) (map2: Map<'k, 'v2>) : Map<'k, ('v1 option * 'v2 option)> =
+
+        let keys = Set.union (keySet map1) (keySet map2)
+
+        keys
+        |> Set.fold
+            (fun (acc: Map<'k, ('v1 option * 'v2 option)>) (k: 'k) ->
+                let v1 = Map.tryFind k map1
+                let v2 = Map.tryFind k map2
+                Map.add k (v1, v2) acc
+            )
+            Map.empty
--- Pinicola.FSharp/Tuple2.fs
+++ Pinicola.FSharp/Tuple2.fs
@@ -6,6 +6,7 @@ module Tuple2 =
     let mapSnd f (a, b) = (a, f b)
     let mapFst f (a, b) = (f a, b)
 
+    let map f (a1, a2) = (f a1, f a2)
     let map1 (f1, f2) (a1, a2) = (f1 a1, f2 a2)
     let map2 (f1, f2) (a1, a2) (b1, b2) = (f1 a1 b1, f2 a2 b2)
 
