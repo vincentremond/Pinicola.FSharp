e09fd83a88106fa075adef1c064dff50eaa65413
Vincent Rémond <vincent.remond@gmail.com>
2025-01-29 15:06:57 +0100

    feat: Add CsvHelper support for CSV file writing
    - Added CsvHelper NuGet package to dependencies
    - Created Csv.fs module with utility functions for writing records to CSV files
    - Implemented writeRecordsToFile and writeRecordsToFileWithCulture functions
    - Updated project references and project file to include new CsvHelper module

---
+++ Pinicola.FSharp/CsvHelper/Csv.fs
@@ -0,0 +1,19 @@
+﻿namespace Pinicola.FSharp.CsvHelper
+
+open System.Globalization
+open System.IO
+open System.Text
+open CsvHelper
+
+[<RequireQualifiedAccess>]
+module Csv =
+
+    let writeRecordsToFileWithCulture<'T> (records: 'T seq) (culture: CultureInfo) (filePath: string) =
+        use writer =
+            new StreamWriter(filePath, (*append*) false, UTF8Encoding( (*encoderShouldEmitUTF8Identifier*) true))
+
+        use csv = new CsvWriter(writer, culture)
+        csv.WriteRecords(records)
+
+    let rec writeRecordsToFile<'T> (records: 'T seq) (filePath: string) =
+        writeRecordsToFileWithCulture records CultureInfo.InvariantCulture filePath
