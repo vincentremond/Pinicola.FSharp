7275d2222ef5285bb270ae9b76785547e7ebebc6
Vincent Rémond <vincent.remond@gmail.com>
2025-05-05 18:35:08 +0200

    feat: Implement withJustification function in PanelHeader module

--- Pinicola.FSharp.SpectreConsole/PanelHeader.fs
+++ Pinicola.FSharp.SpectreConsole/PanelHeader.fs
@@ -4,5 +4,9 @@ open Spectre.Console
 
 [<RequireQualifiedAccess>]
 module PanelHeader =
-    
+
     let fromString (s: string) = PanelHeader(s)
+
+    let withJustification (j: Justify) (header: PanelHeader) =
+        header.Justification <- j
+        header
