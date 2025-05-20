27c0331099d39c0aef2045f295481a473499e1dd
Vincent Rémond <vincent.remond@gmail.com>
2025-05-20 17:16:19 +0200

    feat: Add Padding module for flexible padding configurations

---
+++ Pinicola.FSharp.SpectreConsole/Padding.fs
@@ -0,0 +1,12 @@
+﻿namespace Pinicola.FSharp.SpectreConsole
+
+open Spectre.Console
+
+[<RequireQualifiedAccess>]
+module Padding =
+
+    let fromSize (size: int) = Padding(size)
+
+    let fromHorizontalAndVertical (horizontal: int, vertical: int) = Padding(horizontal, vertical)
+
+    let fromLeftTopRightBottom (left: int) (top: int) (right: int) (bottom: int) = Padding(left, top, right, bottom)
--- Pinicola.FSharp.SpectreConsole/Panel.fs
+++ Pinicola.FSharp.SpectreConsole/Panel.fs
@@ -16,3 +16,11 @@ module Panel =
     let withHeader (header: PanelHeader) (panel: Panel) =
         panel.Header <- header
         panel
+        
+    let withExpand (expand: bool) (panel: Panel) =
+        panel.Expand <- expand
+        panel
+        
+    let withPadding (padding: Padding) (panel: Panel) =
+        panel.Padding <- padding
+        panel
