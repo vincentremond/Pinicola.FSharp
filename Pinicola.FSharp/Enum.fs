293b3eaa797bd5a33a8092883a679089c4a17184
Vincent Rémond <vincent.remond@gmail.com>
2025-06-03 12:45:21 +0200

    feat: Add Enum module with getValues and getFlags functions; include in project file

---
+++ Pinicola.FSharp/Enum.fs
@@ -0,0 +1,12 @@
+﻿namespace Pinicola.FSharp
+
+open System
+
+[<RequireQualifiedAccess>]
+module Enum =
+
+    let getValues<'t when 't :> Enum> () : 't list =
+        Enum.GetValues(typeof<'t>) |> Seq.cast<'t> |> Seq.toList
+
+    let getFlags<'t when 't :> Enum> (value: 't) =
+        () |> getValues<'t> |> List.filter value.HasFlag
