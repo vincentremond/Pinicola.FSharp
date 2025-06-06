c4971d4e1825e6bf065004b2f68b5872311d3cde
Vincent Rémond <vincent.remond@gmail.com>
2025-06-06 19:33:58 +0200

    feat: Add delete function to File module for file deletion

--- Pinicola.FSharp/IO/File.fs
+++ Pinicola.FSharp/IO/File.fs
@@ -8,3 +8,4 @@ module File =
     let writeAllText path (text: string) = File.WriteAllText(path, text)
     let readAllText path = File.ReadAllText(path)
     let exists path = File.Exists(path)
+    let delete = File.Delete
