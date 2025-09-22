881e19dc32aea1af63348e09698995c0ea02b862
Vincent Rémond <vincent.remond@gmail.com>
2025-09-22 13:22:52 +0200

    feat: Add functions for table column width and wrapping configuration

--- Pinicola.FSharp.SpectreConsole/TableColumn.fs
+++ Pinicola.FSharp.SpectreConsole/TableColumn.fs
@@ -1,5 +1,6 @@
 ﻿namespace Pinicola.FSharp.SpectreConsole
 
+open System
 open Spectre.Console
 
 [<RequireQualifiedAccess>]
@@ -7,4 +8,20 @@ module TableColumn =
 
     let withAlignment (justify: Justify) (tableColumn: TableColumn) =
         tableColumn.Alignment <- justify
+        tableColumn
+
+    let withWidth (width: int) (tableColumn: TableColumn) =
+        tableColumn.Width <- width
+        tableColumn
+
+    let unsetWidth (tableColumn: TableColumn) =
+        tableColumn.Width <- Nullable<int>()
+        tableColumn
+
+    let withNoWrap (tableColumn: TableColumn) =
+        tableColumn.NoWrap <- true
+        tableColumn
+
+    let withWrap (tableColumn: TableColumn) =
+        tableColumn.NoWrap <- false
         tableColumn
