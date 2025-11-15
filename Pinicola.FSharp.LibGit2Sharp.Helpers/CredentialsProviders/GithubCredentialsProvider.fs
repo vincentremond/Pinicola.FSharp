4b90a88c343c85963e41887a09e66430aee10a70
Vincent Rémond <vincent.remond@gmail.com>
2025-11-16 00:14:27 +0100

    --wip-- [skip ci]

---
+++ Pinicola.FSharp.LibGit2Sharp.Helpers/CredentialsProviders/GithubCredentialsProvider.fs
@@ -0,0 +1,11 @@
+﻿namespace Pinicola.FSharp.LibGit2Sharp.Helpers
+
+open LibGit2Sharp.Handlers
+
+type MyCredentialsHandler = delegate of 
+
+module GithubCredentialsProvider =
+    let hander: CredentialsHandler =
+        (fun (url, usernameFromUrl, types) -> failwith "not implemented"
+
+        )
